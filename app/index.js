81de177a23b311950d031a6ca03823eb084884d5
quietshu <ds303077135@gmail.com>
2016-05-07 08:54:50 +0800

    Features added: settings & pagination
    - settings
    - pagination
    - POST fields validation

--- app/index.js
+++ app/index.js
@@ -35,27 +35,44 @@ app.use(body());
 
 // view routes
 app.use(route.get('/', function *() {
-  yield this.render('posts', {posts: postAll()});
+  yield this.render('posts', {posts: postAll(1), blog: blogInfo(), current: 1, total: postPageCnt()});
+}));
+
+app.use(route.get('/p/:cnt', function *(cnt) {
+  if (cnt == '1')
+    return this.redirect('/');
+  yield this.render('posts', {posts: postAll(+cnt), blog: blogInfo(), current: +cnt, total: postPageCnt()});
+}));
+
+app.use(route.get('/settings', function *() {
+  yield this.render('settings', {blog: blogInfo()});
+}));
+
+app.use(route.get('/post/new', function *() {
+  yield this.render('post-new', {blog: blogInfo()});
+}));
+
+app.use(route.get('/post/:link', function *(link) {
+  yield this.render('post', {post: post(link), marked: marked, blog: blogInfo()});
 }));
 
 app.use(route.post('/post/new', function *() {
+  let title = this.request.body.title;
+  let content = this.request.body.content;
+  if (!title || !title.length) {
+    this.status = 406;
+    return this.body = 'Post title cannot be empty!';
+  }
+
   let id = (+db.object.id || 0) + 1;
   db.object.id = id;
   db('posts').push({
-    title: this.request.body.title,
-    content: this.request.body.content,
+    title,
+    content,
     link: '' + id,
     time: (new Date()).getTime()
   });
   this.redirect('/');
-}));
-
-app.use(route.get('/post/new', function *() {
-  yield this.render('post-new', {});
-}));
-
-app.use(route.get('/post/:link', function *(link) {
-  yield this.render('post', {post: post(link), marked: marked});
 }));
 
 app.use(route.post('/post/:link/delete', function *(link) {
@@ -74,8 +91,44 @@ app.use(route.post('/post/:link/edit', function *(link) {
   this.redirect('/post/' + link);
 }));
 
+app.use(route.post('/settings', function *() {
+  let name = this.request.body.name;
+  let pagination = this.request.body.pagination;
+  let footer = this.request.body.footer;
+  let googleAnalytics = this.request.body['google-analytics'];
+  let disqus = this.request.body.disqus;
+
+  if (!name || !name.length) {
+    this.status = 406;
+    return this.body = 'Blog name cannot be empty!';
+  }
+
+  if (!pagination || !pagination.length) {
+    pagination = 10; // fallback
+  }
+  try {
+    pagination = ~~Number(pagination); // to integer
+    if (1 > pagination || pagination > 100) {
+      throw new Error();
+    }
+  } catch (err) {
+    console.log(err);
+    pagination = 10; // fallback
+  }
+
+  db.object.blog = {
+    name: name,
+    footer: footer,
+    pagination: pagination,
+    'google-analytics': googleAnalytics || '',
+    disqus: disqus || ''
+  };
+
+  this.redirect('back');
+}));
+
 app.use(route.get('/post/:link/edit', function *(link) {
-  yield this.render('post-edit', {post: post(link)});
+  yield this.render('post-edit', {post: post(link), blog: blogInfo()});
 }));
 
 // static files
@@ -84,12 +137,34 @@ app.use(route.get('/style', function *() {
 }));
 
 // api functions
-function postAll() {
-  return db('posts').orderBy('time', 'desc');
+function postAll(page) {
+  let pagination = db('blog').value().pagination;
+  page = page || 1;
+  return db('posts').chain().orderBy('time', 'desc').take(page * pagination).takeRight(pagination).value();
 }
 
 function post(link) {
   return db('posts').find({link});
+}
+
+function postPageCnt() {
+  let pagination = db('blog').value().pagination;
+  return Math.ceil(db('posts').size() / pagination);
+}
+
+function blogInfo() {
+  return db('blog').value();
+}
+
+// init db
+if (typeof db.object.blog === 'undefined') {
+  db.object.blog = {
+    name: 'innsbruck',
+    footer: 'copyright 2016 © innsbruck',
+    pagination: 10,
+    'google-analytics': '',
+    disqus: ''
+  };
 }
 
 // bind port
